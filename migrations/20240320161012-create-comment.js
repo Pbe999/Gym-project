7557930dbd3dc4c20af548e00b8a98b187ec13e4
Pbe999 <yrjt@fresnou.org>
2024-05-01 08:24:18 -0700

    final commit

---
+++ migrations/20240320161012-create-comment.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      author_name: {
+        type: Sequelize.STRING
+      },
+      body: {
+        type: Sequelize.STRING
+      },
+      commented_on: {
+        type: Sequelize.DATE
+      },
+      article_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Comments');
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
+const articleController = require('../controllers/articleController')
+const commentController = require('../controllers/commentController')
+const userController = require('../controllers/userController')
+const ensureUserAuthenticated = require('../middleware/ensureUserAuthenticated')
+const userHasRole = require('../middleware/userHasRole')
+router.get('/', function (req, res){
+  res.redirect('/article')
+})
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/article/add',ensureUserAuthenticated,userHasRole('author'), articleController.renderAddForm);
+router.post('/article/add',ensureUserAuthenticated,userHasRole('author'), articleController.addArticle);
 
+router.get('/article/:articleId', articleController.displayArticle);
+router.get('/article/', articleController.displayAll);
+
+router.get('/article/:articleId/edit',ensureUserAuthenticated,userHasRole('author'), articleController.renderEditForm);
+router.post('/article/:articleId/edit',ensureUserAuthenticated,userHasRole('author'), articleController.updateArticle);
+
+router.get('/article/:articleId/delete',ensureUserAuthenticated, articleController.deleteArticle);
+router.post('/comment/:commentId/reply/create', commentController.addReply);
+router.get('/register', userController.renderRegistrationForm);
+router.post('/register', userController.register);
+
+router.get('/login', userController.renderLogin);
+router.post('/login', userController.login);
+router.get('/logout', userController.logout);
+router.post('/article/:articleId/comment/create', commentController.createComment);
+router.get('/comment/:commentId/delete',ensureUserAuthenticated, userHasRole('admin'), commentController.deleteComment);
+router.get('/comment/:commentId/reply/:replyId/delete',ensureUserAuthenticated, userHasRole('admin'), commentController.deleteReply);
 module.exports = router;
